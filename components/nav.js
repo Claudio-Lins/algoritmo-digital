e3b6eb47479b4f8a98016db30c1b8801641bd454
clins <klaulins@gmail.com>
2021-02-05 14:23:17 +0000

    Update nav.js

--- components/nav.js
+++ components/nav.js
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-export default function Nav() {
-  return (
+//export default function Nav() {
+  //return (
+    const Nav = () => (
+     
     <nav className='bg-gray-100 py-4'>
       <div className='max-w-5xl px-4 mx-auto'>
         <div className='flex justify-between'>
@@ -24,9 +26,16 @@ export default function Nav() {
                 </Link>
               </div>
               <div>
-                <Link href="/">
+                <Link href="/fonctionnalites">
                   <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
-                    <h3>Home</h3>
+                    <h3>Fonctionnalités</h3>
+                  </a>
+                </Link>
+              </div>
+              <div>
+                <Link href="/qui-sommes-nous">
+                  <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
+                    <h3>Qui sommes-nous ?</h3>
                   </a>
                 </Link>
               </div>
@@ -37,15 +46,15 @@ export default function Nav() {
           </div>
           {/* Mobile Button */}
           <div className='md:hidden flex items-center'>
-            <button>
-          <svg className='w-6 h-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-</svg>
-</button>
+            <button className='mobile-menu-button'>
+              <svg className='w-6 h-6' xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+              </svg>
+            </button>
           </div>
         </div>
         {/* Menu Mobile */}
-        <div>
+        <div className='mobile-menu hidden md:hidden'>
           <div className='ml-4'>
             <Link href="/">
               <a className="text-blue-500 no-underline text-accent-1 dark:text-blue-300">
@@ -63,8 +72,21 @@ export default function Nav() {
         </div>
       </div>
     </nav>
+  
+  // const btn = document.querySelector('button.mobile-menu-button')
+  // const menu = document.querySelector('.mobile-menu')
+
+  // btn.addEventListener("click", () => {
+  //   menu.classList.toggle('hidden');
+  // })
+  
   )
-}
+  export default Nav
+
+ 
+  
+
+ 
 
 
 
